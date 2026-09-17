c7333eb3b17616bd1c4d74319b28858b0d78dc4f
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-09-17 17:18:47 -0300

    Entity Framework - Usando DataAnnotations (DEMO Class Cliente)

--- EntityFramework/Curso/Domain/Cliente.cs
+++ EntityFramework/Curso/Domain/Cliente.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CursoEFCore.Entities
 {
+    [Table("Clientes")] //Informar o nome na tabela
     internal class Cliente
     {
+        //DataAnnotations
+        [Key] //Chave Primaria
         public int Id { get; set; }
+        [Required] //Campo Obrigatorio
         public string Nome { get; set; }
+        [Column("Phone")] //Mapear a propiedade com nome na tabela do BD.
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
